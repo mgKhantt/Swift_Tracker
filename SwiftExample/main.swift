7e3bc1da2b0d2910d193f26c6406ad9d12ab9138
mgKhantt <khantphonenaing156@gamil.com>
2024-11-15 21:31:36 +0630

    Swift Tracker

--- SwiftExample/main.swift
+++ SwiftExample/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+class Track {
+    let name: String
+    let instructor: String
+    
+    init(name: String, instructor: String) {
+        self.name = name
+        self.instructor = instructor
+    }
+}
 
+let tracks: [Track] = [
+    Track(name: "Mobile", instructor: "Tommy"), //0
+    Track(name: "Web", instructor: "Brian"),    //1
+    Track(name: "Game", instructor: "Colton")   //2
+]
+
+let students = ["Harry", "Ron", "Hermione"]
+
+var assigments: [String : Track] = [:]
+
+for student in students {
+    let track = Int.random(in: 0 ..< tracks.count) //random number == 1
+    assigments[student] = tracks[track]  //track = 1 , student = Harry
+    ///assigments = [
+    ///Harry : 1,
+    ///Ron : 1,
+    ///Hermione : 0
+    ///]
+}
+
+for (student, track) in assigments {
+    print("\(student) got \(track.name) with \(track.instructor)")
+}
